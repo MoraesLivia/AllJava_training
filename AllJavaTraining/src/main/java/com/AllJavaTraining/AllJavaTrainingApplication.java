dabdd1f8f9adb18cc18dece0b34ff17d2ec01921
MoraesLivia <liviamoraes49@gmail.com>
2023-10-23 13:43:31 -0300

    feat: initial config

---
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/AllJavaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.AllJavaTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllJavaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllJavaTrainingApplication.class, args);
+	}
+
+}
