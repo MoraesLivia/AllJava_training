263e6d630ab8a744f8a782bf9c8b22e2fe6b47f0
MoraesLivia <liviamoraes49@gmail.com>
2023-11-09 11:34:55 -0300

    feat: add exception method to ModelRepository

--- AllJavaTraining/src/main/java/com/AllJavaTraining/repositories/ModelRepository.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/repositories/ModelRepository.java
@@ -3,5 +3,8 @@ package com.AllJavaTraining.repositories;
 import com.AllJavaTraining.domain.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ModelRepository extends JpaRepository<Model, Integer> {
+    Optional<Model> findModelById(Integer id);
 }
