d60fe2b860b4bc76d776842cfe165d30056cf243
MoraesLivia <liviamoraes49@gmail.com>
2023-11-09 11:26:03 -0300

    feat: add DTO data to Model

--- AllJavaTraining/src/main/java/com/AllJavaTraining/domain/model/Model.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/domain/model/Model.java
@@ -1,6 +1,7 @@
 package com.AllJavaTraining.domain.model;
 
 import com.AllJavaTraining.domain.brand.Brand;
+import com.AllJavaTraining.dtos.ModelDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -15,8 +16,13 @@ public class Model {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String description;
-    @OneToOne
+    private String model_description;
+    @ManyToOne
     @JoinColumn(name = "brand_id")
     private Brand brand;
+
+    public Model (ModelDTO data){
+        this.model_description= data.model_description();
+        this.brand = data.brand();
+    }
 }
