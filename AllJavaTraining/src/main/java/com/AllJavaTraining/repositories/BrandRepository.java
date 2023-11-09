af886df3fe7227d7371e47bb423e75ca126beef8
MoraesLivia <liviamoraes49@gmail.com>
2023-11-09 11:34:38 -0300

    feat: add exception method to BrandRepository

--- AllJavaTraining/src/main/java/com/AllJavaTraining/repositories/BrandRepository.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/repositories/BrandRepository.java
@@ -3,5 +3,8 @@ package com.AllJavaTraining.repositories;
 import com.AllJavaTraining.domain.brand.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    Optional<Brand> findBrandById(Integer id);
 }
