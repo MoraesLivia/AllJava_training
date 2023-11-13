46e0a4a708ad988186b93f0bdc636ce7af2a6000
MoraesLivia <liviamoraes49@gmail.com>
2023-11-13 15:02:36 -0300

    fix: remove empty imports

--- AllJavaTraining/src/main/java/com/AllJavaTraining/controllers/ConcessionaireController.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/controllers/ConcessionaireController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.AllJavaTraining.services.VehicleService;
 
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class ConcessionaireController {
     @Autowired
     private ConcessionaireService concessionaireService;
 
+    @Autowired
+    private VehicleService vehicleService;
+
     //endpoint para listar as concessionárias
     @PostMapping
     public ResponseEntity<Concessionaire> createConcessionaire(@RequestBody ConcessionaireDTO concessionaire){
@@ -25,6 +29,7 @@ public class ConcessionaireController {
 
         //o requestbody é para saber que o data está vindo no corpo da requisição
     }
+
 
     //endpoint para listar as concessionárias para ver se está adicionando da forma correta
     @GetMapping
