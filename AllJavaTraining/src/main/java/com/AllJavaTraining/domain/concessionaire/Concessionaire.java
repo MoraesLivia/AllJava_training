0b36e1512fa22caa93433709a0f91aa49fbca6ca
MoraesLivia <liviamoraes49@gmail.com>
2023-11-13 15:01:19 -0300

    feat: add vehicles list

--- AllJavaTraining/src/main/java/com/AllJavaTraining/domain/concessionaire/Concessionaire.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/domain/concessionaire/Concessionaire.java
@@ -1,8 +1,10 @@
 package com.AllJavaTraining.domain.concessionaire;
+import com.AllJavaTraining.domain.vehicle.Vehicle;
 import com.AllJavaTraining.dtos.ConcessionaireDTO;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.List;
 
 @Entity(name="concessionaire")
 @Table(name= "concessionaire")
@@ -17,6 +19,9 @@ public class Concessionaire {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @OneToMany(mappedBy = "concessionaire", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Vehicle> vehicles;
 
     public Concessionaire(ConcessionaireDTO data){
         this.name = data.name();
--- AllJavaTraining/src/main/java/com/AllJavaTraining/dtos/VehicleDTO.java
+++ AllJavaTraining/src/main/java/com/AllJavaTraining/dtos/VehicleDTO.java
@@ -1,7 +1,8 @@
 package com.AllJavaTraining.dtos;
+import com.AllJavaTraining.domain.concessionaire.Concessionaire;
 import com.AllJavaTraining.domain.model.Model;
 
 import java.util.Date;
 
-public record VehicleDTO(Integer id, String chassi, Model model, Integer fabricationDate, Integer modelDate, Date registerDate) {
+public record VehicleDTO(Integer id, String chassi, Model model, Integer fabricationYear, Integer modelYear, Date registerDate, Concessionaire concessionaire) {
 }
